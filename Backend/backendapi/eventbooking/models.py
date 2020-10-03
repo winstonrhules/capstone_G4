62f442bd57214fdf3471fcc6e0b30091a6ca0c1b
Glory GUEZO-MEVO <60992924+GloryX98@users.noreply.github.com>
2020-10-03 08:25:04 +0000

    New updates Robert

--- Backend/backendapi/eventbooking/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class RegisterEvent(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
-    number = models.IntegerField()
-    event = models.CharField(max_length=50)
--- Backend/backendapi/eventbooking/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import RegisterEvent
-from rest_framework.authtoken.models import Token
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = RegisterEvent
-        fields = ['id','name','email','number','event']
